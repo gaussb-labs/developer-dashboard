a64c7629c0da6a96720fe2eb65885fe626a63de1
SandeepSaini <sandeepsaini85113@gmail.com>
2022-06-14 16:14:23 +0530

    allow users to logout   change root path to login page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
   def new
     @user = User.new
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def index
@@ -12,11 +13,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your accound information was successfully updated"
       redirect_to @user
@@ -28,6 +27,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "You have successfully signed up"
       redirect_to @user
     else
@@ -36,6 +36,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:username, :email, :public_key, :password)
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :users, except: [:new]
-  root "pages#home"
+  root "sessions#new"
   get 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  get 'login', to: 'sessions#destroy'
+  get 'logout', to: 'sessions#destroy'
 end
