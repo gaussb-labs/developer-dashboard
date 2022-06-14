dd0de06b2a322d00189dad45d7d3fc0ce13bc87d
SandeepSaini <sandeepsaini85113@gmail.com>
2022-06-14 13:22:11 +0530

    add byebug and bycrypt gem   create and save user to database with public key

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'pages/home'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'signup', to: 'users#new'
   root "pages#home"
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, except: [:new]
 end
