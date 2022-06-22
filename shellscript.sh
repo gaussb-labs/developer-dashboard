42e32a97c7904de0c5a4ec2ad0f1c890438161f1
SandeepSaini <sandeepsaini85113@gmail.com>
2022-06-22 10:37:12 +0530

    remove ssh command from shellscript.sh

--- shellscript.sh
+++ shellscript.sh
@@ -4,5 +4,4 @@ public_ip=($(aws ec2 describe-instances --query "Reservations[*].Instances[*].Pu
 echo ${public_ip[*]}
 
 ssh-copy-id -i ~/.ssh/users_keys/id_rsa_arihant.pub ubuntu@${public_ip[1]}
-#
-ssh ${public_ip[1]} -l ubuntu
+
